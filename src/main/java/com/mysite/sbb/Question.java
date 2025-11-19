667500175d496b44c2a35b7b3fceb517dbb4eaeb
MOONJUNSEO <answnstj1234@naver.com>
2025-11-19 15:40:55 +0900

    07

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDateTime createDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
