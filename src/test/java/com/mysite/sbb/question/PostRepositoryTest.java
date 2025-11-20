f30839e4cfde4ce1287409de55defb08d1e6b6b2
MOONJUNSEO <answnstj1234@naver.com>
2025-11-20 17:52:41 +0900

    26

--- src/test/java/com/mysite/sbb/question/PostRepositoryTest.java
+++ src/test/java/com/mysite/sbb/question/PostRepositoryTest.java
@@ -1,5 +1,7 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
-class PostRepositoryTests {
+class PostRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
